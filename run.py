e0752d107866779e42c7568c62236513a08b2b5a
Bhawna-Taneja <86619455+Bhawna-Taneja@users.noreply.github.com>
2023-04-27 22:52:59 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
 import streamlit as st
 import streamlit.components.v1 as components
+import os
 
 
-# st.header("index html import")
+files = [file for file in os.listdir("templates/css/") if file.endswith('.css')]
+files2 = [file for file in os.listdir("templates/js/") if file.endswith('.js')]      
 
+source_code = ''
 HtmlFile = open("templates/index.html", 'r', encoding="utf-8")
-source_code = HtmlFile.read() 
+for fl in files:
+    with open("templates/css/"+fl) as f:
+        source_code = source_code + f'<style>{f.read()}</style>'
+        
+for fl in files2:
+    with open("templates/js/"+fl) as f:
+        source_code = source_code + f'<script>{f.read()}</script>'
+        
+source_code = source_code + HtmlFile.read() 
 # print(source_code)
-#components.html(source_code)
-components.html(source_code,height=2500, width=2000, scrolling=False)
+# components.html(source_code)
+components.html(source_code,height=1080, width=1920, scrolling=True)
